9714fe89e305cb96414888f0c14f8004bd1cee6d
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-18 12:12:13 -0500

    Working on bonus for exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -11,8 +11,9 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = (req.getParameter("name"));
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World</h1>");
+        out.println(String.format("Hello, %d", name));
     }
 }
