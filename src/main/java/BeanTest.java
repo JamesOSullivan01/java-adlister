ce4dff9c42995c2b6554fde1fb3eb1019c73df00
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-20 14:40:44 -0500

    Completed exeercise on javabeans

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,7 @@
+public class BeanTest {
+
+    public static void main(String[] args) {
+        albums newAlbum = new albums("Michael Jackson", "Bad", 2001, 23.5, "pop");
+        System.out.println(newAlbum.toString());
+    }
+}
